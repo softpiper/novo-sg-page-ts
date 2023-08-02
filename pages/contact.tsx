2803a4ac2ad0740e2c9e6fafae452629dee63f8f
Al Arafat Siddique <alarafatsiddique@gmail.com>
2023-08-02 15:27:55 +0600

    build error fixed

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -55,7 +55,6 @@ const contact = () => {
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.808662761552!2d103.8316263741724!3d1.2890129617700317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1977d959ad0f%3A0x36811a678ae67b76!2sHoliday%20Inn%20Singapore%20Atrium%2C%20an%20IHG%20Hotel!5e0!3m2!1sen!2sbd!4v1683381402844!5m2!1sen!2sbd"
                 height={350}
                 style={{ border: 0 }}
-                allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               />
